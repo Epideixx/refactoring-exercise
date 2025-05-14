229ba7f9aaccd8a0590c1ac5cc195ce6c0c414bb
Epideixx <jonathan.poliberchet@gmail.com>
2025-05-14 13:38:14 +0200

    Refactored where method - Created read_data function to read file as csv - created find_data_in_csv to find given values for a precise field/column - Directly convert csv to desired output type = list of dictionaries

--- python/funding_raised.py
+++ python/funding_raised.py
@@ -1,64 +1,54 @@
+import pandas as pd
 import csv
 
-class FundingRaised:
-  @staticmethod
-  def where(options = {}):
-    with open("../startup_funding.csv", "rt") as csvfile:
-      data = csv.reader(csvfile, delimiter=',', quotechar='"')
-      # skip header
-      next(data)
-      csv_data = []
-      for row in data:
-        csv_data.append(row)
+def read_data(file_path : str = "../startup_funding.csv"):
+  """Read CSV data and return a DataFrame."""
+  data = pd.read_csv(file_path, delimiter=',', quotechar='"', dtype=str)
+  return data
 
-    funding = []
+def find_data_in_csv(dataframe: pd.DataFrame, column_name: str, value: str) -> pd.DataFrame:
+  """Filter dataframe based on column value and return matching rows as a DataFrame.
+  Args:
+      dataframe (pd.DataFrame): The DataFrame to filter.
+      column_name (str): The name of the column to filter by.
+      value (str): The value to match in the specified column.
+  Returns:
+      pd.DataFrame: A DataFrame containing the rows that match the specified column value.
+  """
+  return dataframe[dataframe[column_name] == value]
+
+class FundingRaised:
+  
+  @staticmethod
+  def where(options = {}, file_path = "../startup_funding.csv"):
+    """Filter the CSV data based on the provided options and return the matching rows.
+    Args:
+        options (dict): A dictionary containing the filter options. The keys can be 'company_name', 'city', 'state', or 'round'.
+        file_path (str): The path to the CSV file. Default is "../startup_funding.csv".
+    Returns:
+        list: A list of dictionaries representing the matching rows in the CSV file.
+    """
+    csv_data = read_data(file_path=file_path)
+
     if 'company_name' in options:
-      result = []
-      for row in csv_data:
-        if row[1] == options['company_name']:
-          result.append(row)
-      csv_data = result
+      csv_data = find_data_in_csv(csv_data, 'company_name', options['company_name'])
 
     if 'city' in options:
-      result = []
-      for row in csv_data:
-        if row[4] == options['city']:
-          result.append(row)
-      csv_data = result
+      csv_data = find_data_in_csv(csv_data, 'city', options['city'])
 
     if 'state' in options:
-      result = []
-      for row in csv_data:
-        if row[5] == options['state']:
-          result.append(row)
-      csv_data = result
+      csv_data = find_data_in_csv(csv_data, 'state', options['state'])
 
     if 'round' in options:
-      result = []
-      for row in csv_data:
-        if row[9] == options['round']:
-          result.append(row)
-      csv_data = result
+      csv_data = find_data_in_csv(csv_data, 'round', options['round'])
 
-    output = []
-    for row in csv_data:
-      mapped = {}
-      mapped['permalink'] = row[0]
-      mapped['company_name'] = row[1]
-      mapped['number_employees'] = row[2]
-      mapped['category'] = row[3]
-      mapped['city'] = row[4]
-      mapped['state'] = row[5]
-      mapped['funded_date'] = row[6]
-      mapped['raised_amount'] = row[7]
-      mapped['raised_currency'] = row[8]
-      mapped['round'] = row[9]
-      output.append(mapped)
+    # Convert the DataFrame to a list of dictionaries
+    output = csv_data.to_dict(orient='records')
 
     return output
 
   @staticmethod
-  def find_by(options):
+  def find_by(options, file_path = "../startup_funding.csv"):
     with open("../startup_funding.csv", "rt") as csvfile:
       data = csv.reader(csvfile, delimiter=',', quotechar='"')
       # skip header
